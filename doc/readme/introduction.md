6f77076b01a0b5faa138e969c4977299b9f97c10
cyberfunk <hello@cyberfunk.org>
2014-09-30 12:57:26 +0800

    Initial files.

---
+++ doc/readme/introduction.md
@@ -0,0 +1,3 @@
+Description parsing for the [command][] module, a component of the [toolkit][].
+
+Represents a string as an object containing the propertes `md` (markdown) and `txt` (plain text).
---
+++ test/spec/description.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect
+  , Description = require('../..');
+
+describe('cli-description:', function() {
+  it('should create description instance', function(done) {
+    done();
+  });
+})
